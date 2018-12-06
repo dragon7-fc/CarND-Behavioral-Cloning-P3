c0e0e0b74d9ef37602974bfa3f6cd737934440d9
dragon7-fc <dragon7.fc@gmail.com>
2018-12-07 00:55:01 +0800

    fix: argparse usage

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ import argparse
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
 
 class Model:
     def __init__(self, args):
@@ -20,7 +22,7 @@ class Model:
         self.batch_size = args.batch_size
         self.learning_rate = args.learning_rate
     
-    def load_data():
+    def load_data(self):
         """
         Load training data and split it into training and validation set
         """
@@ -29,56 +31,73 @@ class Model:
         X = data_df[['center', 'left', 'right']].values
         y = data_df['steering'].values
 
-        self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(X, y, test_size=args.test_size, random_state=0)
+        self.X_train, self.X_valid, self.y_train, self.y_valid = train_test_split(X, y, test_size=self.train_test_split, random_state=0)
 
-    # def train():
+
+    def build_model(self):
+        model = Sequential()
+        model.add(Lambda(lambda x: x/127.5-1.0, input_shape=NNPUT_SHAPE))
+        model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+        model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+        model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+        model.add(Conv2D(64, 3, 3, activation='elu'))
+        model.add(Conv2D(64, 3, 3, activation='elu'))
+        model.add(Dropout(args.keep_prob))
+        model.add(Flatten())
+        model.add(Dense(100, activation='elu'))
+        model.add(Dense(50, activation='elu'))
+        model.add(Dense(10, activation='elu'))
+        model.add(Dense(1))
+        model.summary()
     
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Behavioral Cloning Model')
     parser.add_argument(
-        'data',
+        '--data',
         type=str,
         help='data directory',
         default='data'
     )
     parser.add_argument(
-        'train_test_split',
+        '--train_test_split',
         type=float,
         help='train test split fraction',            
         default=0.2
     )
     parser.add_argument(
-        'dropout',
+        '--dropout',
         type=float, 
         help='drop out probability',          
         default=0.5
     )
     parser.add_argument(
-        'epoch',
+        '--epoch',
         type=int,
         help='number of epochs',
         default=10
     )
     parser.add_argument(
-        'samples_per_epoch',
+        '--samples_per_epoch',
         type=int,
         help='samples per epoch',
         default=20000
         )
     parser.add_argument(
-        'batch_size',
+        '--batch_size',
         type=int,
         help='batch size',
         default=40
     )
     parser.add_argument(
-        'learning_rate',
+        '--learning_rate',
         type=float,
         help='learning rate',
         default=1.0e-4
     )
     args = parser.parse_args()
+    
 
     model = Model(args)
     model.load_data()
+    # model.build_model()
