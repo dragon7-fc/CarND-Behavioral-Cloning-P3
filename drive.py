ffc0da1408fc3df4c05176f17795ecc4a16ecdd1
dragon7-fc <dragon7.fc@gmail.com>
2018-12-09 10:50:57 +0800

    feat: implement model

--- drive.py
+++ drive.py
@@ -13,66 +13,58 @@ from flask import Flask
 from io import BytesIO
 
 from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
+
+from model import Model
 
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
 
+MAX_SPEED = 25
+MIN_SPEED = 10
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
-
-    def set_desired(self, desired):
-        self.set_point = desired
-
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
-
-
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
-
+speed_limit = MAX_SPEED
 
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
         # The current steering angle of the car
-        steering_angle = data["steering_angle"]
+        steering_angle = float(data["steering_angle"])
         # The current throttle of the car
-        throttle = data["throttle"]
+        throttle = float(data["throttle"])
         # The current speed of the car
-        speed = data["speed"]
+        speed = float(data["speed"])
         # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
-
-        throttle = controller.update(float(speed))
-
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
-
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
         # save frame
         if args.image_folder != '':
             timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
             image_filename = os.path.join(args.image_folder, timestamp)
             image.save('{}.jpg'.format(image_filename))
+            
+        try:
+            img = np.asarray(image)      # from PIL image to numpy array
+            img = Model().img_preprocess(img) # apply the preprocessing
+            img = np.array([img])       # the model expects 4D array
+
+            # predict the steering angle for the image
+            steering_angle = float(model.predict(img, batch_size=1))
+            # lower the throttle as the speed increases
+            # if the speed is above the current speed limit, we are on a downhill.
+            # make sure we slow down first and then go back to the original max speed.
+            global speed_limit
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED  # slow down
+            else:
+                speed_limit = MAX_SPEED
+            throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2
+
+            print('{} {} {}'.format(steering_angle, throttle, speed))
+            send_control(steering_angle, throttle)
+        except Exception as e:
+            print(e)
+        
     else:
         # NOTE: DON'T EDIT THIS.
         sio.emit('manual', data={}, skip_sid=True)
@@ -109,15 +101,6 @@ if __name__ == '__main__':
         help='Path to image folder. This is where the images from the run will be saved.'
     )
     args = parser.parse_args()
-
-    # check that model Keras version is same as local Keras version
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
-
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
 
     model = load_model(args.model)
 
--- model.py
+++ model.py
@@ -7,26 +7,41 @@ The script used to create and train the model.
 import argparse
 import pandas as pd
 import os
+import cv2
+import numpy as np
+import matplotlib.image as mpimg
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Lambda, Conv2D, MaxPooling2D, Dropout, Dense, Flatten
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import Adam
 
 class Model:
-    def __init__(self, args):
-        self.data = args.data
-        self.train_test_split = args.train_test_split
-        self.dropout = args.dropout
-        self.epoch = args.epoch
-        self.batch_size = args.batch_size
-        self.samples_per_epoch = args.samples_per_epoch
-        self.batch_size = args.batch_size
-        self.learning_rate = args.learning_rate
+    __IMAGE_HEIGHT=66
+    __IMAGE_WIDTH=200
+    __IMAGE_CHANNEL=3
+    __INPUT_SHAPE = (__IMAGE_HEIGHT, __IMAGE_WIDTH, __IMAGE_CHANNEL)
+        
+    def __init__(self, *args):
+        if args:
+            parse_args = args[0]
+            self.data_dir = parse_args.data_dir
+            self.train_test_split = parse_args.train_test_split
+            self.dropout = parse_args.dropout
+            self.epoch = parse_args.epoch
+            self.batch_size = parse_args.batch_size
+            self.steps_per_epoch = parse_args.steps_per_epoch
+            self.batch_size = parse_args.batch_size
+            self.learning_rate = parse_args.learning_rate
+            self.save_best_only = True
+        else:
+            self.data_dir = 'data'
     
-    def load_data(self):
+    def load_csv(self):
         """
         Load training data and split it into training and validation set
         """
-        data_df = pd.read_csv(os.path.join(args.data, 'driving_log.csv'))
+        data_df = pd.read_csv(os.path.join(self.data_dir, 'driving_log.csv'))
 
         X = data_df[['center', 'left', 'right']].values
         y = data_df['steering'].values
@@ -36,25 +51,202 @@ class Model:
 
     def build_model(self):
         model = Sequential()
-        model.add(Lambda(lambda x: x/127.5-1.0, input_shape=NNPUT_SHAPE))
+        model.add(Lambda(lambda x: x/127.5-1.0, input_shape=self.__INPUT_SHAPE))
         model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
         model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
         model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
         model.add(Conv2D(64, 3, 3, activation='elu'))
         model.add(Conv2D(64, 3, 3, activation='elu'))
-        model.add(Dropout(args.keep_prob))
+        model.add(Dropout(self.dropout))
         model.add(Flatten())
         model.add(Dense(100, activation='elu'))
         model.add(Dense(50, activation='elu'))
         model.add(Dense(10, activation='elu'))
         model.add(Dense(1))
         model.summary()
+        self.model = model
     
+    def load_image(self, filename):
+        """
+        Load RGB images from a file
+        """
+        return mpimg.imread(os.path.join(self.data_dir, filename.strip()))
+
+    def img_crop(self, img):
+        """
+        Crop the image (removing the sky at the top and the car front at the bottom)
+        """
+        return img[60:-25, :, :] # remove the sky and the car front
+
+
+    def img_resize(self, img):
+        """
+        Resize the image to the input shape used by the network model
+        """
+        return cv2.resize(img, (self.__IMAGE_WIDTH, self.__IMAGE_HEIGHT), cv2.INTER_AREA)
+
+
+    def img_rgb2yuv(self, img):
+        """
+        Convert the image from RGB to YUV (This is what the NVIDIA model does)
+        """
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+
+
+    def img_preprocess(self, img):
+        """
+        Combine all preprocess functions into one
+        """
+        img = self.img_crop(img)
+        img = self.img_resize(img)
+        img = self.img_rgb2yuv(img)
+        return img
+
+
+    def __choose_image(self, center, left, right, steering_angle):
+        """
+        Randomly choose an image from the center, left or right, and adjust
+        the steering angle.
+        """
+        choice = np.random.choice(3)
+        if choice == 0:
+            return self.load_image(left), steering_angle + 0.2
+        elif choice == 1:
+            return self.load_image(right), steering_angle - 0.2
+        return self.load_image(center), steering_angle
+
+
+    def img_random_flip(self, img, steering_angle):
+        """
+        Randomly flipt the image left <-> right, and adjust the steering angle.
+        """
+        if np.random.rand() < 0.5:
+            img = cv2.flip(img, 1)
+            steering_angle = -steering_angle
+        return img, steering_angle
+
+
+    def img_random_shift(self, img, steering_angle, range_x, range_y):
+        """
+        Randomly shift the image virtially and horizontally.
+        """
+        shift_x = range_x * (np.random.rand() - 0.5)
+        shift_y = range_y * (np.random.rand() - 0.5)
+        steering_angle += shift_x * 0.002
+        m = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
+        height, width = img.shape[:2]
+        img = cv2.warpAffine(img, m, (width, height))
+        return img, steering_angle
+
+
+    def img_random_shadow(self, img):
+        """
+        Generates and adds random shadow
+        """
+        # (x1, y1) and (x2, y2) forms a line
+        # xm, ym gives all the locations of the image
+        x1, y1 = img.shape[1] * np.random.rand(), 0
+        x2, y2 = img.shape[1] * np.random.rand(), img.shape[0]
+        xm, ym = np.mgrid[0:img.shape[0], 0:img.shape[1]]
+
+        # mathematically speaking, we want to set 1 below the line and zero otherwise
+        # Our coordinate is up side down.  So, the above the line: 
+        # (ym-y1)/(xm-x1) > (y2-y1)/(x2-x1)
+        # as x2 == x1 causes zero-division problem, we'll write it in the below form:
+        # (ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0
+        mask = np.zeros_like(img[:, :, 1])
+        mask[(ym-y1)*(x2-x1) - (y2-y1)*(xm-x1) > 0] = 1
+
+        # choose which side should have shadow and adjust saturation
+        cond = mask == np.random.randint(2)
+        s_ratio = np.random.uniform(low=0.2, high=0.5)
+
+        # adjust Saturation in HLS(Hue, Light   , Saturation)
+        hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
+        return cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
+
+
+    def img_random_brightness(self, img):
+        """
+        Randomly adjust brightness of the image.
+        """
+        # HSV (Hue, Saturation, Value) is also called HSB ('B' for Brightness).
+        hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        ratio = 1.0 + 0.4 * (np.random.rand() - 0.5)
+        hsv[:,:,2] =  hsv[:,:,2] * ratio
+        return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+
+
+    def __augument(self, center, left, right, steering_angle, range_x=100, range_y=10):
+        """
+        Generate an augumented image and adjust steering angle.
+        (The steering angle is associated with the center image)
+        """
+        img, steering_angle = self.__choose_image(center, left, right, steering_angle)
+        img, steering_angle = self.img_random_flip(img, steering_angle)
+        img, steering_angle = self.img_random_shift(img, steering_angle, range_x, range_y)
+        img = self.img_random_shadow(img)
+        img = self.img_random_brightness(img)
+        return img, steering_angle
+
+
+    def __batch_generator(self, filenames, steering_angles, is_training):
+        """
+        Generate training image give image paths and associated steering angles
+        """
+        imgs = np.empty([self.batch_size, self.__IMAGE_HEIGHT, self.__IMAGE_WIDTH, self.__IMAGE_CHANNEL])
+        steers = np.empty(self.batch_size)
+        while True:
+            i = 0
+            for index in np.random.permutation(filenames.shape[0]):
+                center, left, right = filenames[index]
+                steering_angle = steering_angles[index]
+                # augmentation
+                if is_training and np.random.rand() < 0.6:
+                    img, steering_angle = self.__augument(center, left, right, steering_angle)
+                else:
+                    img = self.load_image(center) 
+                # add the image and steering angle to the batch
+                imgs[i] = self.img_preprocess(img)
+                steers[i] = steering_angle
+                i += 1
+                if i == self.batch_size:
+                    break
+            yield imgs, steers
+
+    def train(self):
+        """
+        Train the model
+        """
+        checkpoint = ModelCheckpoint(
+            'model-{epoch:03d}.h5',
+            monitor='val_loss',
+            verbose=0,
+            save_best_only=self.save_best_only,
+            mode='auto'
+        )
+
+        self.model.compile(
+            loss='mean_squared_error',
+            optimizer=Adam(lr=self.learning_rate)
+        )
+
+        self.model.fit_generator(
+            self.__batch_generator(self.X_train, self.y_train, True),
+            self.steps_per_epoch,
+            self.epoch,
+            max_q_size=1,
+            validation_data=self.__batch_generator(self.X_valid, self.y_valid, False),
+            nb_val_samples=len(self.X_valid),
+            callbacks=[checkpoint],
+            verbose=1
+        )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Behavioral Cloning Model')
     parser.add_argument(
-        '--data',
+        '--data_dir',
         type=str,
         help='data directory',
         default='data'
@@ -78,11 +270,11 @@ if __name__ == '__main__':
         default=10
     )
     parser.add_argument(
-        '--samples_per_epoch',
+        '--steps_per_epoch',
         type=int,
-        help='samples per epoch',
+        help='steps per epoch',
         default=20000
-        )
+    )
     parser.add_argument(
         '--batch_size',
         type=int,
@@ -97,7 +289,9 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
     
+    np.random.seed(0)
 
     model = Model(args)
-    model.load_data()
-    # model.build_model()
+    model.load_csv()
+    model.build_model()
+    model.train()
